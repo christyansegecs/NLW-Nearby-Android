75051cabaa5241a52c54f0b5afad2dfd219e803c
christyansegecs <christyansegecs@hotmail.com>
2024-12-24 18:23:35 -0300

    Aula 01

---
+++ app/src/main/java/com/chris/nearby/data/mock/MockCategories.kt
@@ -0,0 +1,22 @@
+package com.chris.nearby.data.mock
+
+import com.chris.nearby.data.model.Category
+
+val mockCategories = listOf(
+    Category(
+        id = "1",
+        name = "Alimentação"
+    ),
+    Category(
+        id = "2",
+        name = "Cinema"
+    ),
+    Category(
+        id = "3",
+        name = "Farmácia"
+    ),
+    Category(
+        id = "4",
+        name = "Supermercado"
+    )
+)
